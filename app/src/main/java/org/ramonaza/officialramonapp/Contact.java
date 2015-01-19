c246ad1b5e069bcae4f9ae5470c6fd73ebb965bd
ofekih <flamingo.ofek@gmail.com>
2015-01-18 19:55:08 -0800

    Added some error-fixing to Contact.java
    Tries to fix incorrect inputs
    eg: "17" -> "2017"
    eg: "9" -> "2019"
    eg: "022" -> "2022"

--- app/src/main/java/org/ramonaza/officialramonapp/Contact.java
+++ app/src/main/java/org/ramonaza/officialramonapp/Contact.java
@@ -18,11 +18,29 @@ public class Contact {
     public Contact(String[] args){
         this.name=args[0];
         this.school=args[1];
-        this.gradYear=args[2];
+        this.gradYear=toYear(args[2]);
         this.address=args[3];
         this.email=args[4];
         this.phoneNumber=args[5];
      }
+    private String toYear(String year)  {   // converts a wrongly formatted year to a correctly formatted year (hopefully)
+        try {                               // eg: "17" to "2017"
+            Integer.parseInt(year);         // if the string is not an integer (eg: "Sophomore"), don't try to make it one
+        } catch (java.lang.NumberFormatException e)  {
+            return year;
+        }
+        Date date = new Date();
+        DateFormat DF = new SimpleDateFormat("yyyy");
+        String curryear = DF.format(date);
+        switch (year.length())  {   // add the correct amount of missing digits to the given year string
+            case 0: return "Not Given";
+            case 1: return curryear.substring(0, 3) + year;
+            case 2: return curryear.substring(0, 2) + year;
+            case 3: return curryear.substring(0, 1) + year;
+            case 4: return year;
+            default: return year.substring(0, 4);
+        }
+    }
     public String getName(){
         return this.name;
     }
@@ -38,11 +56,15 @@ public class Contact {
     public String getAddress(){
         return this.address;
     }
-
     public String getGradYear(){
         return this.gradYear;
     }
     public String getYear() {
+        try {                               // If the aleph entered a string instead of his grad year, display the string
+            Integer.parseInt(gradYear);
+        } catch (java.lang.NumberFormatException e)  {
+            return gradYear;
+        }
         String yearString = "";
         switch (getDifference())    {
             case -1: yearString += "College ";
